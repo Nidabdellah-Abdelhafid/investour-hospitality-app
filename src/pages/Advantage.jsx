6855a5f34789d6ec607cc342c63ee9a07db69305
Nidabdellah-Abdelhafid <nidabdellahabdelhafid99@gmail.com>
2024-10-26 15:16:18 +0100

    c home

--- src/pages/Advantage.jsx
+++ src/pages/Advantage.jsx
@@ -3,31 +3,35 @@ import ScrollToTopButton from './ScrollToTopButton'
 
 function Advantage() {
   return (
-    <div className='m-14'>
-        
-      <div className="breadcrumbs text-sm m-14 p-5">
-      <ul>
-        <li><a>Home</a></li>
-        <li><a>How we work Avontage</a></li>
-      </ul>
-    </div>
-<section className="content-section p-10">
-        <div className="container mx-auto text-center lg:text-left">
-          <h2 className="text-4xl font-bold mb-4">Developing the Future Avontage</h2>
-          <p className="text-lg leading-relaxed mb-6">
-            Investour Hospitality succeeds when you do. Our team looks forward to working with you to understand your hotel development goals, offer our hospitality expertise and support your business objectives to achieve long-term success.
-          </p>
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-            alt="Hotel Development"
-            className="w-full lg:w-1/2 object-cover rounded-lg"
-          />
+    <div className='m-14 py-8'>
+
+      <div className="breadcrumbs text-sm px-6 md:px-36">
+        <ul>
+          <li><a>Home</a></li>
+          <li><a >How we work Avontage</a></li>
+        </ul>
+      </div>
+
+      <section className="content-section">
+        <div className="flex flex-col lg:flex-row">
+          <div className="lg:w-full mb-8 lg:mb-0 lg:pr-12 px-6 md:px-12 lg:px-16 xl:px-32 py-8 lg:py-16">
+            <h1 className="text-2xl md:text-3xl lg:text-5xl font-semibold mb-6 lg:mb-10 text-start" style={{ color: '#281A2A' }}>
+              The Power Of<br/>
+              Investour Hospitality
+            </h1>
+            <div className="flex my-2">
+              <div className="w-1 h-[100px] lg:h-24" style={{ backgroundColor: '#281A2A' }}></div>
+              <p className="text-base md:text-lg lg:text-lg mb-6 font-medium text-start ml-6 lg:ml-10 my-2 mx-2 lg:mx-4" style={{ color: '#281A2A' }}>
+                Investour Hospitality succeeds when you do. Our team looks forward to working with you to understand your hotel development goals, offer our hospitality expertise and support your business objectives to achieve long-term success.
+              </p>
+            </div>
+          </div>
         </div>
       </section>
 
 
       <ScrollToTopButton />
-        </div>
+    </div>
   )
 }
 
